1af50b5535300d6ad34e4f3a2dfeded906113f10
Kafil <kafil@Kafils-MacBook-Pro.local>
2020-06-14 14:05:05 +0100

    Fix default textnode due to change in source
     Author:    Kafil Nasdami <kafil.nasdami@gmail.com>

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   timeOffset: '+0000',
   websiteUrl: 'http://www.covidmaroc.ma/pages/Accueil.aspx',
   textNode:
-    '#WebPartWPQ1 > div.ms-rtestate-field > div:nth-child(2) > table > tbody',
+    '#WebPartWPQ1 > div.ms-rtestate-field > div:nth-child(3) > table > tbody',
   regionsNode: '#WebPartWPQ2 > div.ms-rtestate-field > table > tbody',
 };
