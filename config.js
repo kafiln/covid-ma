c98a209f54fc614966ba7cdf4292e909001ca02b
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-09-25 19:14:59 +0100

    update default website url

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
   errorMessage: 'Invalid node path',
   dateFormat: 'hhmm DD-MM-YYYY Z',
   timeOffset: '+0000',
-  websiteUrl: 'http://www.covidmaroc.ma/pages/Accueil.aspx',
+  websiteUrl: 'http://www.covidmaroc.ma/pages/Accueilfr.aspx',
   textNode:
-    '#WebPartWPQ1 > div.ms-rtestate-field > div:nth-child(3) > table > tbody',
+    '#WebPartWPQ1 > div.ms-rtestate-field > div:nth-child(2) > table > tbody',
   regionsNode: '#WebPartWPQ2 > div.ms-rtestate-field > table > tbody',
 };
